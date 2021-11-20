f1428456fe6ef1e7645703956023fae85f4b35db
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-11-20 07:57:59 -0500

    Enum, processes and IO file system

---
+++ processes.exs
@@ -0,0 +1,55 @@
+# Enum
+list = Enum.map([4, 6, 8, 9], fn x -> x * x end)
+
+IO.puts(inspect(list))
+
+# Filter
+positive? = &(&1 >= 0)
+
+filteredList = Enum.filter(-10..10, positive?)
+
+IO.puts(inspect(filteredList))
+
+# pipe operator
+sum = -10..10 |> Enum.map(&(&1 * 2)) |> Enum.filter(positive?) |> Enum.sum()
+IO.puts("The sum is #{sum}")
+
+# stream
+list2 = -10..10 |> Stream.map(&(&1 * 2)) |> Stream.filter(positive?)
+IO.puts(inspect(list2))
+IO.puts("\n")
+
+# Task
+IO.puts("Starting asynchronous task")
+
+task =
+  Task.async(fn ->
+    IO.puts("waiting respond")
+    :timer.sleep(5000)
+    IO.puts("Message from asynchronous task")
+  end)
+
+Task.await(task, 7000)
+IO.puts("Message after asynchronous task")
+
+# Reading files
+IO.puts("The file content is")
+stream = File.stream!("file.txt")
+
+%File.Stream{
+  line_or_bytes: :line,
+  modes: [:raw, :read_ahead, :binary],
+  path: "file.txt",
+  raw: true
+}
+
+IO.puts(inspect(Enum.take(stream, 10)))
+
+IO.puts(inspect(File.read!("file.txt")))
+
+IO.puts(Path.join("hello", "world"))
+
+IO.puts("")
+
+IO.puts("the file path of file.txt is #{Path.expand("~/file.txt")}")
+IO.gets("Do you want continue?\nyes or not?")
