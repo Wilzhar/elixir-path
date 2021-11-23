01e9569791e991d717cf446cc7106904d001c668
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-11-23 14:56:38 -0500

    Erlang modules

--- error_mechanisms.exs
+++ error_mechanisms.exs
@@ -35,3 +35,61 @@ error3 =
   end
 
 IO.puts(error3)
+
+# Erlang libraries
+
+# returns Unicode codepoint
+IO.puts(inspect(String.to_charlist("Ø")))
+
+# deals with raw data bytes
+IO.puts(inspect(:binary.bin_to_list("Ø")))
+
+# Crypto module
+IO.puts(inspect(Base.encode16(:crypto.hash(:sha256, "Wilmar Valencia"))))
+
+# Diagraph module
+digraph = :digraph.new()
+coords = [{0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}]
+[v0, v1, v2] = for c <- coords, do: :digraph.add_vertex(digraph, c)
+:digraph.add_edge(digraph, v0, v1)
+:digraph.add_edge(digraph, v1, v2)
+short_path = :digraph.get_short_path(digraph, v0, v2)
+
+IO.puts(inspect(short_path))
+
+# Erlang Term Storage
+table = :ets.new(:ets_test, [])
+
+:ets.insert(table, {"China", 1_374_000_000})
+:ets.insert(table, {"India", 1_284_000_000})
+:ets.insert(table, {"USA", 322_000_000})
+:ets.i(table)
+
+# math module
+IO.puts(:math.pi())
+IO.puts(:math.sin(90))
+IO.puts(:math.exp(4))
+IO.puts(:math.pow(2, 8))
+
+# queue module
+queue = :queue.new()
+queue = :queue.in("First", queue)
+queue = :queue.in("Second", queue)
+queue = :queue.in("Third", queue)
+
+{value, queue} = :queue.out(queue)
+IO.puts(inspect(value))
+
+{value, queue} = :queue.out(queue)
+IO.puts(inspect(value))
+
+{value, queue} = :queue.out(queue)
+IO.puts(inspect(value))
+
+{value, queue} = :queue.out(queue)
+IO.puts(inspect(value))
+
+# rand module
+IO.puts(:rand.uniform())
+IO.puts(:rand.uniform())
+IO.puts(:rand.uniform())
