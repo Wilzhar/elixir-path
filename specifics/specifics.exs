02d14a155223fe87db5ca7c365df7e1b85b9adb5
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-11-30 14:11:41 -0500

    Implement EEx

---
+++ specifics/specifics.exs
@@ -0,0 +1,18 @@
+defmodule Example do
+  require EEx
+  EEx.function_from_file(:def, :greeting, "greeting.eex", [:name])
+end
+
+IO.inspect(Example.greeting("Sean"))
+
+IO.inspect(EEx.eval_string("Hi, <%= @name %>", assigns: [name: "Sean"]))
+
+if_statement = """
+<%= if condition do %>
+  <%= a - b %>
+<% else %>
+  <%= b - a %>
+<% end %>
+"""
+
+IO.inspect(EEx.eval_string(if_statement, condition: false, a: 2, b: 1))
